9e00e7b111e54001db23555c1ef0f165acd868e3
calltpj@gmail.com <tulashiprasad@Tulashis-MacBook-Air.local>
2022-02-13 17:09:47 +0545

    Inital Commit

---
+++ smart_meter/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login),
+    path('index', views.index),
+    path('admin', views.admin) ,
+    path('contact', views.contact) ,
+    path('signup/', views.user_signup) ,
+    path('login/', views.user_login),
+    path('changepw/', views.changepw),
+    path('logout/', views.app_logout) ,
+    path('notification/', views.notification) ,
+]
