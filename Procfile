53ba6ecd5b245929f6f0d462000f12f4aa70a31a
calltpj@gmail.com <tulashiprasad@Tulashis-MacBook-Air.local>
2022-02-13 19:44:39 +0545

    heroku changes 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn minor_project.wsgi --log file -
--- minor_project/settings.py
+++ minor_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^jv&3f%+oaexi0&-ztzigox^r5-tmy5l%pxvrp3=f3wz0tu16p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysmartmeter.herokuapp.com','127.0.0.1']
 
 
 # Application definition
